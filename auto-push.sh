1d011798559c3c118897d6c5ae9763cbe646c83f
hslee137@lotte.net <hslee137>
2019-11-28 12:36:34 +0900

    [2019-11-28] M  auto-push.sh

--- auto-push.sh
+++ auto-push.sh
@@ -2,9 +2,15 @@
 
 git pull
 
-commit_message="[$(date +%Y-%m-%d)] $1"
+arg_messge="$1"
+git add . -A
 append_message="$(git status --short)"
 
-git add . -A
-git commit -m "$commit_message" -m "$append_message"
+if [ -z "$commit_message" ]
+then
+    git commit -m "[$(date +%Y-%m-%d)] $append_message"
+else
+    git commit -m "[$(date +%Y-%m-%d)] $commit_message" -m "$append_message"
+fi
+
 git push
